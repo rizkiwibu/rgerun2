42fc8dffb13ef7626940216d2c53e3c3569128e2
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-06-23 07:20:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 //=============『 Utama 』================== //
-global.owner = ['6285878836361'] 
+global.owner = ['6283160624440'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'iky'
-global.numberowner = '6285878836361'
+global.nameowner = 'REGHY BOTZ'
+global.numberowner = '6283160624440'
 global.instagram = 'https://instagram.com/ikykunnnn'
 global.github = 'https://github.com/rizkiwibu'
 global.dana = '6283817161861'
@@ -13,7 +13,7 @@ global.pulsa = '6283817161861'
 global.gopay = '6283817161861'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ikyBot-Md'
+global.namebot = 'REGHY BOTZ'
 global.gc = 'https://chat.whatsapp.com/KcMIbBUddqjIoNmGCrRU5m'
 global.web = 'https://www.helgastoree.xyz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
@@ -23,10 +23,10 @@ global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti ja
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'iky Beta'
+global.wm = 'REGHY BOTZ'
 global.watermark = wm
-global.wm2 = '                     「 iky Beta あ⁩ 」'
-global.wm3 = '⫹⫺ iky Beta'
+global.wm2 = '                     「 REGHY BOTZ あ⁩ 」'
+global.wm3 = '⫹⫺ REGHY BOTZ'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = '2022'
-global.author = '© Ikybot'
+global.author = '© REGHY BOTZ'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
